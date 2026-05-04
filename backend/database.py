c010c7fa3a4ddb572b10fd2c6d80936cf2b8b9da
bellelynn <18024534686@qq.com>
2026-05-04 20:20:25 +1000

    Add users collection with unique email index

--- backend/database.py
+++ backend/database.py
@@ -15,11 +15,12 @@ client = None
 db = None
 products_collection = None
 cart_collection = None
+users_collection = None
 
 
 async def connect_db():
     """Initializes the MongoDB connection"""
-    global client, db, products_collection, cart_collection
+    global client, db, products_collection, cart_collection, users_collection
 
     # Get connection string from .env
     mongodb_url = os.getenv("MONGODB_URL")
@@ -33,6 +34,10 @@ async def connect_db():
     # Assign collections to global variables
     products_collection = db.products
     cart_collection = db.cart_items
+    users_collection = db.users
+
+    # Ensure email is unique at the database level
+    await users_collection.create_index("email", unique=True)
 
     print("Connected to MongoDB!")
 
@@ -57,3 +62,10 @@ def get_cart_collection():
     if db is None:
         return None
     return db.cart_items
+
+
+def get_users_collection():
+    """Returns the users collection dynamically"""
+    if db is None:
+        return None
+    return db.users
